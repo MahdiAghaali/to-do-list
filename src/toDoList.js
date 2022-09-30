e81b7d1b5c31a4ca4a5af23f9c8b02df5ba4b48f
Mahdi Aghaali <Mahdi.aghaali@yahoo.com>
2022-09-30 12:58:20 +0330

    Add functionality for selecting a task and toggle

--- src/toDoList.js
+++ src/toDoList.js
@@ -9,3 +9,22 @@
     this.updateList();
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  selectTask(index) {
+    const tasks = document.querySelectorAll('.task');
+    tasks.forEach((task, i) => {
+      task.classList.remove('selected');
+      if (index === i) {
+        task.classList.add('selected');
+      }
+    });
+  }
+
+  toggleTask(i) {
+    if (this.tasks[i].isCompleted === true) {
+      this.tasks[i].isCompleted = false;
+    } else {
+      this.tasks[i].isCompleted = true;
+    }
+    this.updateList();
+  }
